a678db73c77d7f0266eadb26b9ac4dc29cb582cc
Miguel Lopez <dudetru25@gmail.com>
2026-07-13 14:36:26 -0400

    Normalize generated notice output

--- native/generate-notices.sh
+++ native/generate-notices.sh
@@ -8,3 +8,4 @@ cargo about generate about.hbs --fail -o "$notice"
 # Some upstream license bodies use CRLF or retain trailing spaces. Normalize the
 # generated artifact so Git diffs and Unity packages remain deterministic.
 perl -pi -e 's/\r$//; s/[ \t]+$//' "$notice"
+perl -0777 -pi -e 's/\n+\z/\n/' "$notice"
